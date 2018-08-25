1d8daf470d50673bfe27b95aa6d6bf98d0b0f64b
McKenzie Fiege <mckenziefiege@McKenzies-MBP.home>
2018-08-25 15:12:36 -0400

    Fixed second method

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,16 @@
-def countdown(number)
-  while number > 0
-    puts "#{number} SECOND(S)!"
-    number -= 1
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
   end
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(number)
-  While number > 0
-    puts "#{number} SECOND(S)"
-    number -= 1
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
     sleep(1)
+    seconds_to_midnight -= 1
   end
   "HAPPY NEW YEAR!"
 end
